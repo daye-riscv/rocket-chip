6ef9c622ee11fa8b082c07c2971e4ff6aa097823
Henry Cook <henry@i0.internal.sifive.com>
2016-07-12 18:18:49 -0700

    WIP more wmask helpers

--- uncore/src/main/scala/tilelink2/Converter.scala
+++ uncore/src/main/scala/tilelink2/Converter.scala
@@ -26,8 +26,8 @@ class UncachedTileLink0To2Converter(implicit p: Parameters) extends tilelink2.Ti
   val gntcnt = tilelink2.TileLinkBeatCounter(ognt)
 
   val oacq_atype = {
-  import tilelink2.ProtocolTransactionTypes._
-  import tilelink.Acquire._
+    import tilelink2.ProtocolTransactionTypes._
+    import tilelink.Acquire._
     MuxLookup[UInt,UInt](iacq.bits.a_type, GET, Seq(
       getType       -> GET,
       getBlockType  -> GET,
@@ -38,8 +38,19 @@ class UncachedTileLink0To2Converter(implicit p: Parameters) extends tilelink2.Ti
       putPrefetchType -> HINT))
   } 
 
-  def wmaskForNonPuts(acq: tilelink.AcquireMetadata): UInt = UInt(0) // TODO
-  val oacq_wmask = Mux(iacq.bits.isPut(), iacq.bits.wmask(), wmaskForNonPuts(iacq.bits))
+  val oacq_wmask = {
+    import tilelink.Acquire._
+    val full = ~UInt(0, tlWmaskWidth)
+    //val partial = iacq.bits.op_size(), iacq.bits.addr_byte() TODO for small Gets?
+    MuxLookup[UInt,UInt](iacq.bits.a_type, iacq.bits.wmask(), Seq(
+      getType       -> full,
+      getBlockType  -> full,
+      putType       -> iacq.bits.wmask(),
+      putBlockType  -> iacq.bits.wmask(),
+      putAtomicType -> iacq.bits.wmask(),
+      getPrefetchType -> full,
+      putPrefetchType -> full))
+  }
 
   def atomicOpCodeHelper(acq: tilelink.AcquireMetadata): UInt = {
     import tilelink2.AtomicOpCodes._
@@ -65,31 +76,57 @@ class UncachedTileLink0To2Converter(implicit p: Parameters) extends tilelink2.Ti
       putBlockType  -> iacq.bits.allocate(), // TODO: contiguous
       putAtomicType -> atomicOpCodeHelper(iacq.bits),
       getPrefetchType -> tilelink2.HintPolicySpecific(should = true, write = false),
-      putPrefetchType -> tilelink2.HintPolicySpecific(should = true, write = true)))
+      putPrefetchType -> tilelink2.HintPolicySpecific(should = true, write = true))).toBits
   } 
 
+  def wmaskToSize(acq: tilelink.AcquireMetadata): (UInt, UInt) = {
+    val (_, put_offset, put_size) = wmaskToSizeHelper((~acq.wmask()).toBools, 0)
+    (put_offset, put_size)
+  }
+
+  def wmaskToSizeHelper(all_inside_0: Seq[Bool], defsize: Int): (Seq[Bool], UInt, UInt) = {
+    val len = all_inside_0.size
+    if (len == 1) {
+      (Seq(Bool(true)), UInt(0), UInt(defsize))
+    } else {
+      val sub_inside_0 = Seq.tabulate (len/2) { i => all_inside_0(2*i) && all_inside_0(2*i+1) }
+      val (sub_outside_0, sub_offset, sub_size) = wmaskToSizeHelper(sub_inside_0, defsize+1)
+      val all_outside_0 = Seq.tabulate (len) { i => sub_outside_0(i/2) && all_inside_0(i^1) }
+      val odd_outside_0 = Seq.tabulate (len/2) { i => all_outside_0(2*i+1) }
+      val odd_outside = odd_outside_0.reduce (_ || _)
+      val all_outside = all_outside_0.reduce (_ || _)
+      val offset = Cat(sub_offset, odd_outside.toBits)
+      val size = Mux(all_outside, UInt(defsize), sub_size)
+      (all_outside_0, offset, size)
+    }
+  }
+
+  val (put_offset, put_size) = wmaskToSize(iacq.bits)
   val lg2CacheBlockBytes = log2Up(tlCacheBlockBytes)
-  def wmaskToSize(acq: tilelink.AcquireMetadata): UInt = UInt(0) // TODO
+  val is_multibeat = iacq.bits.hasMultibeatData()
   val oacq_size = {
     import tilelink.Acquire._
     MuxLookup(iacq.bits.a_type, UInt(0), Seq(
       getType       -> iacq.bits.op_size(),
       getBlockType  -> UInt(lg2CacheBlockBytes),
-      putType       -> wmaskToSize(iacq.bits),
+      putType       -> put_size,
       putBlockType  ->  UInt(lg2CacheBlockBytes),
       putAtomicType -> iacq.bits.op_size(),
       getPrefetchType -> UInt(lg2CacheBlockBytes),
       putPrefetchType -> UInt(lg2CacheBlockBytes)))
   } 
 
+  val oacq_addr = iacq.bits.full_addr() |
+                    Mux(iacq.bits.a_type === tilelink.Acquire.putType, put_offset, UInt(0))
+
   iacq.ready := oacq.ready
   oacq.valid := iacq.valid
   oacq.bits := tilelink2.Acquire(
     a_type = oacq_atype,
     source = Cat(iacq.bits.client_id, iacq.bits.client_xact_id),
-    addr = iacq.bits.full_addr(),
+    addr = oacq_addr,
     wmask = oacq_wmask,
-    union = oacq_union.toBits,
+    union = oacq_union,
     sz = oacq_size,
     data =  iacq.bits.data
   )
